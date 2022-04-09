703c51d27ff2d40deca9c4b64dc15c7ee9c10d77
Pieter Huizenga <mail@pieterhuizenga.com>
2022-04-09 21:53:53 +0200

    Theme now switches instantly on pref change

--- nebula/static/js/darkmode.js
+++ nebula/static/js/darkmode.js
@@ -6,12 +6,21 @@ let colorsSource = getComputedStyle(document.documentElement);
 let colorScheme = ['--color-background', '--color-background-secondary', '--color-text-primary', '--color-text-secondary', 
     '--color-text-tertiary', '--color-text-accent', '--color-primary', '--color-secondary','--color-accent']
 
-if (prefersDarkMode.matches) {
+const setColorScheme = e => {
+  if (e.matches) {
+    // Dark
+    console.log('Dark mode')
     colorScheme.forEach(function (color) {
         document.documentElement.style.setProperty(color, colorsSource.getPropertyValue(color + "-dark"));
     });
-} else {    
+  } else {
+    // Light
+    console.log('Light mode')
     colorScheme.forEach(function (color) {
         document.documentElement.style.setProperty(color, colorsSource.getPropertyValue(color + "-light"));
     });
+  }
 }
+  
+setColorScheme(prefersDarkMode);
+prefersDarkMode.addEventListener('change', setColorScheme);
