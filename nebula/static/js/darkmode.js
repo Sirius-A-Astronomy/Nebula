f8fcfd5d4e5ceadce92ab22b3ee8c96a2f48ef8f
Pieter Huizenga <mail@pieterhuizenga.com>
2022-04-11 21:10:31 +0200

    Docs: explained color-scheme

--- nebula/static/js/darkmode.js
+++ nebula/static/js/darkmode.js
@@ -3,20 +3,19 @@ const prefersDarkMode = window.matchMedia('(prefers-color-scheme: dark)');
 // set colorsCourse as the :root css variable
 let colorsSource = getComputedStyle(document.documentElement);
 
+// Add colors that should respond to the prefers-color-scheme media query here:
 let colorScheme = ['--color-background', '--color-background-secondary', '--color-text-primary', '--color-text-secondary', 
     '--color-text-tertiary', '--color-text-accent', '--color-text-on-accent', 
     '--color-text-accent-focus', '--color-primary', '--color-secondary','--color-accent'];
 
 const setColorScheme = e => {
   if (e.matches) {
-    // Dark
-    console.log('Dark mode')
+    // Set each color variable to it's respective color in the dark color scheme
     colorScheme.forEach(function (color) {
         document.documentElement.style.setProperty(color, colorsSource.getPropertyValue(color + "-dark"));
     });
   } else {
-    // Light
-    console.log('Light mode')
+    // Set each color variable to it's respective color in the light color scheme
     colorScheme.forEach(function (color) {
         document.documentElement.style.setProperty(color, colorsSource.getPropertyValue(color + "-light"));
     });
