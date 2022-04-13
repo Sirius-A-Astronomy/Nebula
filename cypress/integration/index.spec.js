f407c9f8dd413b6232641285075a325efd37f993
Pieter Huizenga <l>
2022-04-13 14:21:31 +0200

    Test: added mobile test

--- cypress/integration/index.spec.js
+++ cypress/integration/index.spec.js
@@ -1,42 +1,76 @@
 //testing nav and index page
 
 describe("index", () => {
-    beforeEach(() => {
-        cy.visit("/");
-        cy.get("h2").should("contain", "Browse");
+    context('on desktop', () => {
+        beforeEach(() => {
+            cy.viewport(1200, 800)
+            cy.visit("/");
+            cy.get("h2").should("contain", "Browse");
+        });
+
+        it("should have a title", () => {
+            cy.title().should("contain", "Nebula");
+        });
+
+        it("should display all courseLevels", () => {
+            cy.get("a").should("contain", "First Year");
+            cy.get("a").should("contain", "Second Year");
+            cy.get("a").should("contain", "Third Year");
+            cy.get("a").should("contain", "General");
+        }
+        );
+        it("should have navigation to a course_level from browse", () => {
+            cy.get("p > a").contains("First Year").click();
+            cy.url().should("include", "/course_levels/bsc-yr1");
+        });
+
+        it("should have navigation to a course_level from nav", () => {
+            cy.get(".navbar-nav").contains("Courses").realHover();
+            cy.get(".dropdown-menu").contains("First Year").click();
+            cy.url().should("include", "/course_levels/bsc-yr1");
+        });
+
+        it("should have navigation to a course from nav", () => {
+            cy.get("a").contains("Courses").realHover();
+            cy.get(".dropdown-menu > li > a").contains("First Year").realHover();
+            cy.get(".dropdown-submenu > li > a").contains("Calculus 1").click();
+            cy.url().should("contain", "/levels/bsc-yr1/courses/WBMA003-05");
+        });
+
+        it("Should have navigation back to the main sirius a website", () => {
+            cy.get("a").contains("Sirius").should("have.attr", "href", "https://sirius.astro.rug.nl/");
+        });
     });
 
-    it("should have a title", () => {
-        cy.title().should("contain", "Nebula");
-    });
+    context('on mobile', () => {
+        beforeEach(() => {
+            cy.viewport('iphone-xr')
+            cy.visit("/");
+            cy.get("h2").should("contain", "Browse");
+        });
 
-    it("should display all courseLevels", () => {
-        cy.get("a").should("contain", "First Year");
-        cy.get("a").should("contain", "Second Year");
-        cy.get("a").should("contain", "Third Year");
-        cy.get("a").should("contain", "General");
-    }
-    );
-    it("should have navigation to a course_level from browse", () => {
-        cy.get("p > a").contains("First Year").click();
-        cy.url().should("include", "/course_levels/bsc-yr1");
-    });
+        it('should have a title', () => {
+            cy.title().should("contain", "Nebula");
+        });
 
-    it("should have navigation to a course_level from nav", () => {
-        cy.get(".navbar-nav").contains("Courses").realHover();
-        cy.get(".dropdown-menu").contains("First Year").click();
-        cy.url().should("include", "/course_levels/bsc-yr1");
-    });
+        it('should display all courseLevels', () => {
+            cy.get("a").should("contain", "First Year");
+            cy.get("a").should("contain", "Second Year");
+            cy.get("a").should("contain", "Third Year");
+            cy.get("a").should("contain", "General");
+        });
 
-    it("should have navigation to a course from nav", () => {
-        cy.get("a").contains("Courses").realHover();
-        cy.get(".dropdown-menu > li > a").contains("First Year").realHover();
-        cy.get(".dropdown-submenu > li > a").contains("Calculus 1").click();
-        cy.url().should("contain", "/levels/bsc-yr1/courses/WBMA003-05");
-    });
+        it('should have navigation to a course_level from browse', () => {
+            cy.get("p > a").contains("First Year").click();
+            cy.url().should("include", "/course_levels/bsc-yr1");
+        });
 
-    it("Should have navigation back to the main sirius a website", () => {
-        cy.get("a").contains("Sirius").should("have.attr", "href", "https://sirius.astro.rug.nl/");
+        it('should have navigation to a course from nav', () => {
+            cy.get(".navbar-toggler").click();
+            cy.get(".navbar-nav").contains("Courses").click();
+            cy.get(".dropdown-menu").contains("First Year").click();
+            cy.get(".dropdown-submenu").contains("Calculus 1").click();
+            cy.url().should("contain", "/levels/bsc-yr1/courses/WBMA003-05");
+        });
     });
-
 });
