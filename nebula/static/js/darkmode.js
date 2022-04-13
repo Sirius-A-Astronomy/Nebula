838ca4a8b2081015e58b2b9e92d0cf05dc9d434e
Pieter Huizenga <l>
2022-04-13 15:29:15 +0200

    Feat: made matchmedia more backwards compatible

--- nebula/static/js/darkmode.js
+++ nebula/static/js/darkmode.js
@@ -1,26 +1,34 @@
-const prefersDarkMode = window.matchMedia('(prefers-color-scheme: dark)');
 
 // set colorsCourse as the :root css variable
 let colorsSource = getComputedStyle(document.documentElement);
 
 // Add colors that should respond to the prefers-color-scheme media query here:
-let colorScheme = ['--color-background', '--color-background-secondary', '--color-text-primary', '--color-text-secondary', 
-    '--color-text-tertiary', '--color-text-accent', '--color-text-on-accent', 
-    '--color-text-accent-focus', '--color-primary', '--color-secondary','--color-accent'];
+let colorScheme = ['--color-background', '--color-background-secondary', '--color-text-primary', '--color-text-secondary',
+  '--color-text-tertiary', '--color-text-accent', '--color-text-on-accent',
+  '--color-text-accent-focus', '--color-primary', '--color-secondary', '--color-accent'];
 
 const setColorScheme = e => {
   if (e.matches) {
     // Set each color variable to it's respective color in the dark color scheme
     colorScheme.forEach(function (color) {
-        document.documentElement.style.setProperty(color, colorsSource.getPropertyValue(color + "-dark"));
+      document.documentElement.style.setProperty(color, colorsSource.getPropertyValue(color + "-dark"));
     });
   } else {
     // Set each color variable to it's respective color in the light color scheme
     colorScheme.forEach(function (color) {
-        document.documentElement.style.setProperty(color, colorsSource.getPropertyValue(color + "-light"));
+      document.documentElement.style.setProperty(color, colorsSource.getPropertyValue(color + "-light"));
     });
   }
 }
-  
-setColorScheme(prefersDarkMode);
-prefersDarkMode.addEventListener('change', setColorScheme);
+
+
+if (window.matchMedia) {
+
+  const prefersDarkMode = window.matchMedia('(prefers-color-scheme: dark)');
+  setColorScheme(prefersDarkMode);
+  if (prefersDarkMode?.addEventListener) {
+    prefersDarkMode.addEventListener('change', setColorScheme);
+  } else {
+    prefersDarkMode.addListener(setColorScheme);
+  }
+}
