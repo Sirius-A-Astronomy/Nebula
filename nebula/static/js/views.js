cd6a052f2609c4d1b86d92c4b4ab1298331c19ed
Pieter Huizenga <mail@pieterhuizenga.com>
2022-04-27 20:51:06 +0200

    Feat: Add comment form to question page

--- nebula/static/js/views.js
+++ nebula/static/js/views.js
@@ -1,73 +1,103 @@
 // expandableText
 document
-  .querySelectorAll(".expandable-text")
-  .forEach(function (expandableText) {
-    const expandableTextContent = expandableText.innerHTML
-      .trim()
-      .replace(/\s+/g, " ");
+	.querySelectorAll(".expandable-text")
+	.forEach(function (expandableText) {
+		const expandableTextContent = expandableText.innerHTML
+			.trim()
+			.replace(/\s+/g, " ");
 
-    let displayLimit;
-    if (expandableText?.getAttribute("displaylimit") !== null) {
-      displayLimit = expandableText.getAttribute("displaylimit");
-    } else {
-      displayLimit = 220;
-    }
+		let displayLimit;
+		if (expandableText?.getAttribute("displaylimit") !== null) {
+			displayLimit = expandableText.getAttribute("displaylimit");
+		} else {
+			displayLimit = 220;
+		}
 
-    let punctIndexes = [
-      ...expandableTextContent.matchAll(new RegExp("[.!?,]", "gi")),
-    ].map((a) => a.index);
-    let punctIndex =
-      punctIndexes.reduce(function (prev, curr) {
-        return Math.abs(curr - displayLimit) < Math.abs(prev - displayLimit)
-          ? curr
-          : prev;
-      }) + 1;
+		if (expandableTextContent.length < displayLimit) {
+			return;
+		}
 
-    let spaceIndexes = [
-      ...expandableTextContent.matchAll(new RegExp(" ", "gi")),
-    ].map((a) => Math.abs(a.index));
-    let spaceIndex = spaceIndexes.reduce(function (prev, curr) {
-      return Math.abs(curr - displayLimit) < Math.abs(prev - displayLimit)
-        ? curr
-        : prev;
-    });
+		let punctIndexes = [
+			...expandableTextContent.matchAll(new RegExp("[.!?,]", "gi")),
+		].map((a) => a.index);
 
-    // If the distance between the displayLimit and punctuation is more than 25% of the displayLimit, break at the nearest space.
-    displayLimit =
-      Math.abs(punctIndex - displayLimit) <
-      Math.abs(spaceIndex - displayLimit) + 0.25 * displayLimit
-        ? punctIndex
-        : spaceIndex;
+		// @TODO stupidly large number, should probably find a more elegant way to do this
+		let punctIndex = 1_000_000_000;
+		if (punctIndexes.length > 0) {
+			punctIndex =
+				punctIndexes.reduce(function (prev, curr) {
+					return Math.abs(curr - displayLimit) <
+						Math.abs(prev - displayLimit)
+						? curr
+						: prev;
+				}) + 1;
+		}
+		let spaceIndexes = [
+			...expandableTextContent.matchAll(new RegExp(" ", "gi")),
+		].map((a) => Math.abs(a.index));
+		let spaceIndex = spaceIndexes.reduce(function (prev, curr) {
+			return Math.abs(curr - displayLimit) < Math.abs(prev - displayLimit)
+				? curr
+				: prev;
+		});
 
-    if (displayLimit >= expandableTextContent.length) {
-      return;
-    }
+		// If the distance between the displayLimit and punctuation is more than 25% of the displayLimit, break at the nearest space.
+		if (Math.abs(punctIndex - displayLimit) > displayLimit / 4) {
+			punctIndex = spaceIndex;
+		}
 
-    const expandTextToggle = document.createElement("a");
-    expandTextToggle.setAttribute("style", "font-size: 0.8125");
-    expandTextToggle.setAttribute("href", "#");
-    expandTextToggle.classList.add("inline-link");
-    expandTextToggle.innerHTML = "Read more";
-    const expandableTextContentTruncated =
-      expandableTextContent.substring(0, displayLimit) + " (...) ";
-    let isTextExpanded = false;
+		// Only change the displayLimit if a suitable breakpoint is found.
+		if (
+			punctIndex < 1.25 * displayLimit ||
+			spaceIndex < 1.25 * displayLimit
+		) {
+			displayLimit = punctIndex;
+		}
 
-    expandTextToggle.addEventListener("click", function () {
-      if (isTextExpanded) {
-        expandableText.innerHTML = expandableTextContentTruncated;
-        expandableText.blur();
-        expandableText.appendChild(expandTextToggle);
-        isTextExpanded = false;
-        expandTextToggle.innerHTML = "Read more";
-      } else {
-        expandableText.innerHTML = expandableTextContent + " ";
-        expandableText.appendChild(expandTextToggle);
-        expandableText.blur();
-        isTextExpanded = true;
-        expandTextToggle.innerHTML = "Read less";
-      }
-    });
+		const expandTextToggle = document.createElement("a");
+		expandTextToggle.setAttribute("style", "font-size: 0.8125");
+		expandTextToggle.setAttribute("href", "#!");
+		expandTextToggle.classList.add("inline-link");
+		expandTextToggle.innerHTML = "Read more";
+		const expandableTextContentTruncated =
+			expandableTextContent.substring(0, displayLimit) + " (...) ";
+		let isTextExpanded = false;
 
-    expandableText.innerHTML = expandableTextContentTruncated;
-    expandableText.appendChild(expandTextToggle);
-  });
+		expandTextToggle.addEventListener("click", function () {
+			if (isTextExpanded) {
+				expandableText.innerHTML = expandableTextContentTruncated;
+				expandableText.blur();
+				expandableText.appendChild(expandTextToggle);
+				isTextExpanded = false;
+				expandTextToggle.innerHTML = "Read more";
+			} else {
+				expandableText.innerHTML = expandableTextContent + " ";
+				expandableText.appendChild(expandTextToggle);
+				expandableText.blur();
+				isTextExpanded = true;
+				expandTextToggle.innerHTML = "Read less";
+			}
+		});
+
+		expandableText.innerHTML = expandableTextContentTruncated;
+		expandableText.appendChild(expandTextToggle);
+	});
+
+// Show submit button on form focus
+document
+	.querySelectorAll(".hidden-form-field-control")
+	.forEach(function (formField) {
+		formField.addEventListener("focus", function () {
+			formField.parentElement
+				.querySelectorAll(".hidden-form-field-element")
+				.forEach((element) => {
+					element.classList.remove("visually-hidden");
+				});
+			// if the elements that need to be shown are not a sibling of the control form field, show them via the data-hidden-by attribute
+			document
+				.querySelectorAll(`[data-hidden-by='${formField.id}']`)
+				.forEach((element) => {
+					element.classList.remove("visually-hidden");
+				});
+		});
+	});
--- nebula/views/question.py
+++ nebula/views/question.py
@@ -1,10 +1,44 @@
-from flask import Blueprint, render_template, url_for, request
-from nebula.models import Course, Question
+from flask import Blueprint, render_template, url_for, request, redirect
+from wtforms import Form, SubmitField, TextAreaField, BooleanField
+from wtforms.validators import DataRequired
+from nebula import db
+from nebula.models import Course, Question, Comment, User
 
-bp = Blueprint('question', __name__, url_prefix='/course/<course_code>/question')
+bp = Blueprint('question', __name__,
+               url_prefix='/course/<course_code>/question')
 
-@bp.route('/<question_id>')
-def question(course_code, question_id):
-    question = Question.query.filter_by(id = question_id).first()
-    course = Course.query.filter_by(code = course_code).first()
-    return render_template('main/question.html', course = course, question = question)
+
+class CommentForm(Form):
+    class Meta:
+        locales = ['en_US', 'en']
+    content = TextAreaField('Add a comment', validators=[
+                            DataRequired()])
+    is_suggestion = BooleanField('Comment is a suggestion')
+    submit = SubmitField('Submit')
+
+
+@bp.route('/<question_uuid>', methods=['GET', 'POST'])
+def question(course_code, question_uuid, new_comment_uuid=None):
+    new_comment_uuid = (request.args.get('new_comment_uuid'))
+    # if new_comment_uuid:
+    #     new_comment_uuid = int(new_comment_uuid)
+
+    comment_form = CommentForm(request.form)
+    question = Question.query.filter_by(uuid=question_uuid).first()
+    course = Course.query.filter_by(code=course_code).first()
+
+    # for now we'll hardcode the user as there is no login system
+    user = User.query.filter_by(username="sipma").first()
+    print(type(Comment.query.first().uuid))
+
+    if request.form and comment_form.validate():
+        content = comment_form.content.data
+        is_suggestion = comment_form.is_suggestion.data
+        comment = Comment(
+            content=content, is_suggestion=is_suggestion, user=user, question=question)
+
+        db.session.add(comment)
+        db.session.commit()
+
+        return redirect(url_for('question.question', course_code=course_code, question_uuid=question_uuid, new_comment_uuid=str(comment.uuid)))
+    return render_template('main/question.html', course=course, question=question, comment_form=comment_form, new_comment_uuid=new_comment_uuid)
