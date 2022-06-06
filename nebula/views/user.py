5de6d8ab98bd79e76ba5c7992ba6f107c7898403
Pieter Huizenga <mail@pieterhuizenga.com>
2022-06-06 15:11:43 +0200

    Feat: add basic password change functionality

--- nebula/views/user.py
+++ nebula/views/user.py
@@ -12,7 +12,7 @@ import re
 
 from wtforms import PasswordField, StringField, SubmitField, Form, ValidationError
 from wtforms.validators import DataRequired, EqualTo, Email
-from flask import Blueprint, render_template, request, redirect, url_for, session, jsonify
+from flask import Blueprint, render_template, request, redirect, url_for, session, flash
 from flask_login import login_required, login_user, current_user, logout_user
 from passlib.hash import sha256_crypt
 
@@ -225,9 +225,83 @@ def logout():
     return redirect(url_for("main.index"))
 
 
-@ bp.route("/profile")
+class EditProfileForm(Form):
+    """Form for editing a user's profile."""
+    first_name = StringField(
+        "First Name", validators=[DataRequired(
+            "Please enter your first name")])
+    last_name = StringField(
+        "Last Name", validators=[DataRequired(
+            "Please enter your last name")])
+    email = StringField(
+        "Email", validators=[DataRequired(
+            "Please enter your email address"), validate_email])
+    edit_submit = SubmitField("Submit changes")
+
+
+class ChangePasswordForm(Form):
+    """Form for changing a user's password."""
+    current_password = PasswordField(
+        "Current Password", validators=[DataRequired(
+            "Please enter your current password")])
+    new_password = PasswordField(
+        "New Password", validators=[DataRequired(
+            "Please enter a new password"),
+            EqualTo("new_password_confirm",
+                    message="Please double check that your passwords match")])
+    new_password_confirm = PasswordField(
+        "Confirm New Password", validators=[DataRequired(
+            "Please confirm your new password")])
+    change_password_submit = SubmitField("Change Password")
+
+
+class ChangeUsernameForm(Form):
+    """Form for changing a user's username."""
+    new_username = StringField(
+        "New Username", validators=[DataRequired(
+            "Please enter a new username"),
+            validate_username])
+    password = PasswordField(
+        "Password", validators=[DataRequired(
+            "Please enter your password")])
+    change_username_submit = SubmitField("Change Username")
+
+
+@ bp.route("/profile", methods=["GET", "POST"])
 @ login_required
 def profile():
     """Creates the profile page for the current user."""
+    change_password_form = ChangePasswordForm(request.form)
+    change_username_form = ChangeUsernameForm(request.form)
+    edit_profile_form = EditProfileForm(request.form)
 
-    return render_template("main/profile.html")
+    if request.method == "POST":
+        if change_password_form.change_password_submit.data == True:
+            if not change_password_form.validate():
+                return render_template("main/profile.html",
+                                       change_password_form=change_password_form,
+                                       change_username_form=change_username_form,
+                                       edit_profile_form=edit_profile_form)
+
+            current_password = change_password_form.current_password.data
+
+            authenticated_user = authenticate(current_user.username,
+                                              current_password)
+            if not authenticated_user:
+                change_password_form.current_password.errors.append(
+                    "That doesn't seem to be your current password")
+                return render_template("main/profile.html",
+                                       change_password_form=change_password_form,
+                                       change_username_form=change_username_form,
+                                       edit_profile_form=edit_profile_form)
+
+            new_password = change_password_form.new_password.data
+            new_password_hashed = sha256_crypt.encrypt(new_password)
+            authenticated_user.password = new_password_hashed
+            db.session.commit()
+            flash("Password changed successfully")
+            return redirect(url_for("user.profile"))
+
+    return render_template("main/profile.html", change_password_form=change_password_form,
+                           change_username_form=change_username_form,
+                           edit_profile_form=edit_profile_form)
