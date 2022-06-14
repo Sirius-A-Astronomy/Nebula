87b0bc6ad82c47a79002478f2a51a50e1ad8a780
Pieter Huizenga <mail@pieterhuizenga.com>
2022-06-14 16:07:15 +0200

    UI: update dashboard page

--- nebula/views/dashboard.py
+++ nebula/views/dashboard.py
@@ -5,7 +5,7 @@ from flask import Blueprint, render_template, request, redirect, url_for, flash
 from sqlalchemy import func
 from flask_login import login_required, current_user
 
-from nebula.models import User, Comment, Course, CourseLevel, Question
+from nebula.models import User, Comment, Course, CourseLevel, Question, Answer
 from nebula import db
 
 bp = Blueprint('dashboard', __name__, url_prefix='/dashboard')
@@ -13,11 +13,17 @@ bp = Blueprint('dashboard', __name__, url_prefix='/dashboard')
 
 @bp.route('/')
 def index():
-    if current_user.is_anonymous and current_user.access_level < 2:
-        flash("You need to be a KLC or Cosmic Web Member to access this page.", 'danger')
+    if current_user.is_anonymous or current_user.access_level < 2:
+        flash("You need to be a KLC or Cosmic Web Member to access this page.", 'warning')
         return redirect(url_for('main.index'))
 
     questions = Question.query.all()
+    answers = len(Answer.query.all())
+    answered_questions = len(
+        Question.query.filter(Question.answers.any()).all())
+
+    reviewed_questions = len(Question.query.filter(
+        Question.approved != 0).all())
     users = User.query.all()
 
     recent_user_count = len([1 if
@@ -25,4 +31,7 @@ def index():
 
     print(recent_user_count)
 
-    return render_template('dashboard/index.html', questions=questions, recent_user_count=recent_user_count)
+    return render_template(
+        'dashboard/index.html', questions=questions,
+        recent_user_count=recent_user_count, reviewed_questions=reviewed_questions,
+        answers=answers, answered_questions=answered_questions)
