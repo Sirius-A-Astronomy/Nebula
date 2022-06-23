98750643f22cbdf62d7148a73c840d6e3e6a19b6
Pieter Huizenga <mail@pieterhuizenga.com>
2022-06-23 16:53:37 +0200

    Fix: darkmode not showing toggle

--- nebula/static/js/darkmode.js
+++ nebula/static/js/darkmode.js
@@ -1,89 +1,63 @@
-// set colorsCourse as the :root css variable
-// const colorsSource = getComputedStyle(document.documentElement);
-
-// Add colors that should respond to the prefers-color-scheme media query here:
-// const colorScheme = [
-// 	"--color-primary",
-// 	"--color-secondary",
-// 	"--color-tertiary",
-// 	"--color-background",
-// 	"--color-background-secondary",
-// 	"--color-background-tertiary",
-// 	"--color-text-primary",
-// 	"--color-text-secondary",
-// 	"--color-text-tertiary",
-// 	"--color-text-on-primary",
-// 	"--color-text-on-accent",
-// 	"--color-accent-focus",
-// 	"--color-accent",
-// 	"--color-form-is-valid",
-// 	"--color-form-is-invalid",
-// ];
-
-const darkModeToggle = document.getElementById("dark-mode-toggler");
+let darkModeToggle;
 
 const initialPrefersDarkMode = () => {
 	if (window.matchMedia) {
 		const darkModePreference = window.matchMedia(
 			"(prefers-color-scheme: dark)"
 		);
-		if (darkModePreference.matches && darkModeToggle) {
-			darkModeToggle.checked = true;
-		}
 		return darkModePreference.matches ? "dark" : "light";
 	}
 	return "light";
 };
 
-function setDarkModeonLoad() {
-	let prefersDarkMode = localStorage.getItem("prefersDarkMode");
+let prefersDarkMode;
+function setDarkModeDuringLoad() {
+	prefersDarkMode = localStorage.getItem("prefersDarkMode");
 	if (prefersDarkMode === null) {
 		prefersDarkMode = initialPrefersDarkMode();
 		localStorage.setItem("prefersDarkMode", prefersDarkMode);
 	}
 	setDarkMode(prefersDarkMode);
 }
-setDarkModeonLoad();
-
-if (darkModeToggle) {
-	darkModeToggle.addEventListener("click", function () {
-		let currentDarkModePreference = localStorage.getItem("prefersDarkMode");
-		if (currentDarkModePreference != "light") {
-			localStorage.setItem("prefersDarkMode", "light");
-			setDarkMode("light");
-		} else {
-			localStorage.setItem("prefersDarkMode", "dark");
-			setDarkMode("dark");
-		}
-		darkModeToggle.classList.add("animate-rotation");
-	});
-}
+setDarkModeDuringLoad();
 
 function setDarkMode(mode) {
-	// colorScheme.forEach(function (color) {
-	// 	document.documentElement.style.setProperty(
-	// 		color,
-	// 		colorsSource.getPropertyValue(color + "-" + mode)
-	// 	);
-	// });
-	// document.documentElement.style.setProperty("color-scheme", mode);
-
-	if (darkModeToggle) {
-		darkModeToggle.checked = mode == "dark";
-	}
 	if (mode == "dark") {
-		if (darkModeToggle) {
-			darkModeToggle.classList.add("dark-mode-enabled");
-			darkModeToggle.classList.remove("light-mode-enabled");
-		}
-
 		document.documentElement.classList.add("dark-mode");
 	} else {
-		if (darkModeToggle) {
-			darkModeToggle.classList.remove("dark-mode-enabled");
-			darkModeToggle.classList.add("light-mode-enabled");
-		}
-
 		document.documentElement.classList.remove("dark-mode");
+	}
+	setDarkModeToggleState(mode);
+}
+
+window.onload = (event) => {
+	darkModeToggle = document.getElementById("dark-mode-toggler");
+	darkModeToggle.classList.add("animate-rotation");
+	setDarkModeToggleState(prefersDarkMode);
+
+	if (!darkModeToggle) {
+		return;
+	}
+	darkModeToggle.addEventListener("click", function () {
+		let currentDarkModePreference =
+			localStorage.getItem("prefersDarkMode") != "light"
+				? "dark"
+				: "light";
+		let newDarkModePreference =
+			currentDarkModePreference != "light" ? "light" : "dark";
+		localStorage.setItem("prefersDarkMode", newDarkModePreference);
+		setDarkMode(newDarkModePreference);
+	});
+};
+
+function setDarkModeToggleState(state) {
+	if (!darkModeToggle) return;
+	darkModeToggle.checked = state == "dark";
+	if (state != "light") {
+		darkModeToggle.classList.add("dark-mode-enabled");
+		darkModeToggle.classList.remove("light-mode-enabled");
+	} else {
+		darkModeToggle.classList.remove("dark-mode-enabled");
+		darkModeToggle.classList.add("light-mode-enabled");
 	}
 }
