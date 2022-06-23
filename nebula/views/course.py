1d1bca6c1eb1baec66326f8262f7e50cb9d25263
Pieter Huizenga <mail@pieterhuizenga.com>
2022-06-23 17:39:39 +0200

    Fix: only send approved questions to front end

--- nebula/views/course.py
+++ nebula/views/course.py
@@ -1,5 +1,7 @@
 from flask import Blueprint, render_template, url_for, request
+from sqlalchemy import and_
 from nebula.models import Course, CourseLevel, Question
+
 
 # Does a course need to be a sublevel of courseLevel?
 # level dependent blueprint:
@@ -13,6 +15,7 @@ bp = Blueprint('course', __name__, url_prefix='/q/<course_level_code>')
 def course(course_code, course_level_code):
     course = Course.query.filter_by(code=course_code).first()
     course_level = CourseLevel.query.filter_by(code=course_level_code).first()
-    questions = Question.query.filter_by(course=course).all()
+    questions = Question.query.filter(
+        and_(Question.reviewed == 1, Question.course_uuid == course.uuid)).all()
 
     return render_template('main/course.html', course=course, course_level=course_level, questions=questions)
