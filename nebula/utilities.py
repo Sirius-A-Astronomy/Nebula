3d47381bf9da0282df1ff4e631131981022f79c8
Pieter Huizenga <mail@pieterhuizenga.com>
2022-10-25 16:34:46 +0200

    Feat: add ability to change access levels of users

--- nebula/utilities.py
+++ nebula/utilities.py
@@ -5,3 +5,12 @@ def unauthorized_handler():
     """Redirects to the login page if the user is not logged in."""
     flash("Please log in to access this page.", "warning")
     return redirect(url_for('user.login_register', next=request.full_path))
+
+
+ACCESS_LEVELS = [
+    {"name": "Guest", "value": 0},
+    {"name": "Student", "value": 1},
+    {"name": "Moderator", "value": 2},
+    {"name": "Admin", "value": 3},
+    {"name": "Maintainer", "value": 4},
+]
