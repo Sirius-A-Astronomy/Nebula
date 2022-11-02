13bee54b1c48478d55f0d4c339a653f7e83a6951
Pieter Huizenga <mail@pieterhuizenga.com>
2022-11-02 12:34:08 +0100

    Stop the page loading animation when all css and js is loaded

--- nebula/static/js/stoppageloadanim.js
+++ nebula/static/js/stoppageloadanim.js
@@ -1,10 +1,8 @@
-window.addEventListener("load", (event) => {
-	document.querySelectorAll(".loader").forEach((loader) => {
+document.querySelectorAll(".loader").forEach((loader) => {
+	setTimeout(() => {
+		loader.classList.add("fade-out");
 		setTimeout(() => {
-			loader.classList.add("fade-out");
-			setTimeout(() => {
-				loader.remove();
-			}, 200);
+			loader.remove();
 		}, 200);
-	});
+	}, 200);
 });
