ac42040e568e0973dffb881900fdef1291bed6a2
Pieter Huizenga <mail@pieterhuizenga.com>
2022-12-24 18:26:11 +0100

    Use Tailwind for utility color utility classes
    This means that we can now have tree-shaking for the colors we use.

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,25 @@
 module.exports = {
 	content: ["./index.html", "./nebula/src/**/*.{vue,js,ts,jsx,tsx}"],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				"primary-clr": "var(--color-primary)",
+				"secondary-clr": "var(--color-secondary)",
+				"tertiary-clr": "var(--color-tertiary)",
+				"accent-clr": "var(--color-accent)",
+				"primary-active": "var(--color-primary-active)",
+
+				"primary-text": "var(--color-primary-text)",
+				"secondary-text": "var(--color-secondary-text)",
+				"tertiary-text": "var(--color-tertiary-text)",
+
+				"on-primary-text": "var(--color-text-on-primary)",
+
+				"primary-bg": "var(--color-background)",
+				"secondary-bg": "var(--color-background-secondary)",
+				"tertiary-bg": "var(--color-background-tertiary)",
+			},
+		},
 		container: {
 			center: true,
 			padding: "1rem",
