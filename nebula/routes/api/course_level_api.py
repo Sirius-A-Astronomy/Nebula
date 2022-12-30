5756e25f37ee8db4c86c6b32842ad802ee58a7ba
Pieter Huizenga <mail@pieterhuizenga.com>
2022-12-30 01:37:45 +0100

    Add API endpoints for courses and course levels

---
+++ nebula/routes/api/course_level_api.py
@@ -0,0 +1,24 @@
+from nebula.routes.api import bp as api_bp
+from nebula.models.course_level import CourseLevel
+
+from flask import Blueprint, jsonify
+
+bp = Blueprint("course_level_api", __name__, url_prefix="/course_levels")
+
+api_bp.register_blueprint(bp)
+
+
+@bp.route("/", methods=["GET"])
+def get_course_levels():
+    course_levels = CourseLevel.query.all()
+    return jsonify(
+        [
+            {
+                "id": course_level.uuid,
+                "name": course_level.name,
+                "code": course_level.code,
+                "study_type": course_level.study_type,
+            }
+            for course_level in course_levels
+        ]
+    )
--- nebula/routes/api/courses_api.py
+++ nebula/routes/api/courses_api.py
@@ -1,7 +1,8 @@
 from flask import Blueprint, jsonify, request
 
-from nebula import csrf
+from nebula import csrf, db
 from nebula.models.course import Course
+from nebula.models.course_level import CourseLevel
 from nebula.routes.api import bp as api_bp
 
 bp = Blueprint("course_api", __name__, url_prefix="/courses")
@@ -14,41 +15,100 @@ def get_courses():
     courses = Course.query.all()
     return jsonify(
         [
-            {
-                "id": course.uuid,
-                "name": course.name,
-                "code": course.code,
-                "course_level": {
-                    "id": course.course_level.uuid,
-                    "name": course.course_level.name,
-                    "code": course.course_level.code,
-                    "study_type": course.course_level.study_type,
-                },
-                "description": course.description,
-            }
-            for course in courses
+            course.expose() for course in courses
         ]
     )
+
+
+@bp.route("/<uuid>", methods=["GET"])
+def get_course(uuid):
+    course = Course.query.filter_by(uuid=uuid).one_or_none()
+    if course is None:
+        return jsonify({"message": "course not found"}), 404
+
+    return jsonify(course.expose())
+
+@bp.route("/<uuid>", methods=["PUT"])
+def update_course(uuid):
+    course = Course.query.filter_by(uuid=uuid).one_or_none()
+    if course is None:
+        return jsonify({"message": "Course not found"}), 404
+
+    data = request.get_json(silent=True)
+
+    name = data.get("name")
+    if name is not None:
+        course.name = name
+
+    code = data.get("code")
+    if code is not None:
+        course.code = code
+
+    description = data.get("description")
+    if description is not None:
+        course.description = description
+
+    course_level_id = data.get("course_level").get("id") if data.get("course_level") is not None else None
+    if course_level_id is not None:
+        if (CourseLevel.query.filter_by(uuid=course_level_id).one_or_none() is None):
+            return jsonify({"message": "Course level does not exist"}), 400
+        course.course_level_uuid = course_level_id
+
+    semester = data.get("semester")
+    if semester is not None:
+        if not semester in ["1", "1a", "1b", "2", "2a", "2b"]:
+            return jsonify({"message": "Semester must be 1, 1a, 1b, 2, 2a or 2b"}), 400
+        course.semester = semester
+
+    db.session.commit()
+
+    course = Course.query.filter_by(uuid=uuid).one_or_none()
+
+
+    return jsonify(course.expose())
 
 
 @bp.route("/", methods=["POST"])
 def create_course():
     data = request.get_json()
-    return jsonify({"POST:": data})
+
+    name = data.get("name")
+    code = data.get("code")
+    description = data.get("description")
+    course_level_id = data.get("course_level").get("id") if data.get("course_level") is not None else None
+    semester = data.get("semester")
+
+    if not name or not code or not course_level_id or not semester:
+        return jsonify({"message": "Missing required fields"}), 400
+
+    if (Course.query.filter_by(code=code).one_or_none() is not None):
+        return jsonify({"message": "Course code already exists"}), 400
+
+    if not semester in ["1", "1a", "1b", "2", "2a", "2b"]:
+        return jsonify({"message": "Semester must be 1, 1a, 1b, 2, 2a or 2b"}), 400
+
+    if (CourseLevel.query.filter_by(uuid=course_level_id).one_or_none() is None):
+        return jsonify({"message": "Course level does not exist"}), 400
+
+    course = Course(name=name, code=code, description=description, course_level_uuid=course_level_id, semester=semester)
+    db.session.add(course)
+    db.session.commit()
+
+    return jsonify(course.expose()), 201
 
 
-@bp.route("/", methods=["PUT"])
+@bp.route("/<uuid>", methods=["DELETE"])
 @csrf.exempt
-def update_course():
-    data = request.get_json()
-    return jsonify({"PUT:": data})
+def delete_course(uuid):
+    course = Course.query.filter_by(uuid=uuid).one_or_none()
+    if course is None:
+        return jsonify({"message": "Course not found"}), 404
 
+    data = course.expose()
+    db.session.delete(course)
+    db.session.commit()
 
-@bp.route("/", methods=["DELETE"])
-@csrf.exempt
-def delete_course():
-    data = request.get_json()
-    return jsonify({"DELETE:": data})
+    return jsonify(data), 200
 
 
 @bp.route("/ping", methods=["post", "GET"])
