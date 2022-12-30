9526d91f869fd3ff80dcea543e5d825575986174
Pieter Huizenga <mail@pieterhuizenga.com>
2022-12-30 11:49:32 +0100

    Fix an issue where the user would be redirected to the login page while logged in

--- nebula/src/js/dashboard/dashboardMain.ts
+++ nebula/src/js/dashboard/dashboardMain.ts
@@ -1,17 +1,20 @@
-// try to login, start as early as possible
-try {
-	await fetchUser();
-} catch (e) {
-	console.log("Error fetching logged in user", e);
-}
-
+import { fetchUser, FetchUserFailedError } from "@/stores/sessionStore";
 import Dashboard from "@/dashboard/Dashboard.vue";
 import { createApp } from "vue";
 import router from "@/router";
-import { fetchUser, FetchUserFailedError } from "@/stores/sessionStore";
 
-const app = createApp(Dashboard);
+// try to login, start as early as possible
+// using IIFE to have "top level" await
+(async () => {
+	try {
+		await fetchUser();
+	} catch (e) {
+		console.log("Error fetching logged in user", e);
+	}
 
-app.use(router);
+	const app = createApp(Dashboard);
 
-app.mount("#app");
+	app.use(router);
+
+	app.mount("#app");
+})();
