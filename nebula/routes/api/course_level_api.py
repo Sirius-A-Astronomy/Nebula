fb19d49f17c54735fbf510d550330d342852a83e
Pieter Huizenga <mail@pieterhuizenga.com>
2023-01-07 00:10:12 +0100

    Add api routes for questions and course levels

--- nebula/routes/api/course_level_api.py
+++ nebula/routes/api/course_level_api.py
@@ -1,5 +1,7 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, request
+from flask_login import current_user
 
+from nebula import db
 from nebula.models.course_level import CourseLevel
 from nebula.routes.api import bp as api_bp
 
@@ -13,12 +15,40 @@ def get_course_levels():
     course_levels = CourseLevel.query.all()
     return jsonify(
         [
-            {
-                "id": course_level.uuid,
-                "name": course_level.name,
-                "code": course_level.code,
-                "study_type": course_level.study_type,
-            }
-            for course_level in course_levels
+            course_level.expose() for course_level in course_levels
         ]
     )
+
+@bp.route("/<uuid>", methods=["GET"])
+def get_course_level(uuid):
+    course_level = CourseLevel.query.filter_by(uuid=uuid).one_or_none()
+    if course_level is None:
+        return jsonify({"message": "course level not found"}), 404
+
+    return jsonify(course_level.expose())
+
+@bp.route("/<uuid>", methods=["PUT"])
+def update_course_level(uuid):
+    if not current_user.is_authenticated or not current_user.access_level >= 3:
+        return jsonify({"message": "unauthorized"}), 401
+
+    course_level = CourseLevel.query.filter_by(uuid=uuid).one_or_none()
+    if course_level is None:
+        return jsonify({"message": "Course level not found"}), 404
+
+    data = request.get_json(silent=True)
+
+    name = data.get("name")
+    if name is not None:
+        course_level.name = name
+
+    code = data.get("code")
+    if code is not None:
+        course_level.code = code
+
+    study_type = data.get("study_type")
+    if study_type is not None:
+        course_level.study_type = study_type
+
+    db.session.commit()
+    return jsonify(course_level.expose())
