31c6081985dbe88dc138036deb23449005c48002
Pieter Huizenga <mail@pieterhuizenga.com>
2023-01-07 01:34:01 +0100

    Return unauthorized message as JSON

--- nebula/utilities.py
+++ nebula/utilities.py
@@ -1,4 +1,4 @@
-from flask import flash, redirect, request, url_for
+from flask import flash, redirect, request, url_for, jsonify
 
 
 def unauthorized_handler():
@@ -6,7 +6,7 @@ def unauthorized_handler():
         request.path.startswith("/api/")
         or request.headers.get("content-type") == "application/json"
     ):
-        return "Unauthorized", 401
+        return jsonify({"message" : "Unauthorized"}), 401
     """Redirects to the login page if the user is not logged in."""
     flash("Please log in to access this page.", "warning")
     return redirect(url_for("web.user.login_register", next=request.full_path))
