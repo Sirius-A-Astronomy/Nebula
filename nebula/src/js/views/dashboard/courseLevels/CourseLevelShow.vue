3e796b9c3a73511aff37cb498b90b93a5265d03f
Pieter Huizenga <mail@pieterhuizenga.com>
2023-01-13 09:43:00 +0100

    Add showing and editing course levels

---
+++ nebula/src/js/views/dashboard/courseLevels/CourseLevelShow.vue
@@ -0,0 +1,152 @@
+<script setup lang="ts">
+import { courseLevelStore } from "@stores/courseLevelStore";
+
+import type { CourseLevel } from "@/stores/courseLevelStore";
+import useFlashStore from "@stores/flashStore";
+import useModalStore from "@stores/modalStore";
+import { ref, computed, watch, onMounted } from "vue";
+import { useRouter } from "vue-router";
+import type { Updatable } from "@/stores/factory/storeFactory";
+import CourseLevelForm from "@components/courseLevel/CourseLevelForm.vue";
+
+const flash = useFlashStore();
+
+const modal = useModalStore();
+
+const props = defineProps<{
+	id: string;
+}>();
+
+const courseLevel = computed(
+	() => courseLevelStore.getters.byId(props.id).value
+);
+
+const router = useRouter();
+
+watch(props, (value) => {
+	loading.value = true;
+	loadData();
+});
+
+const loading = ref(true);
+
+const loadData = async () => {
+	if (!courseLevel.value) {
+		await courseLevelStore.actions.getById(props.id);
+	}
+
+	loading.value = false;
+};
+
+const awaitingResponse = ref(false);
+
+const updateCourseLevel = async (courseLevel: Updatable<CourseLevel>) => {
+	awaitingResponse.value = true;
+	const response = await courseLevelStore.actions.update(courseLevel);
+	awaitingResponse.value = false;
+
+	if (response.status !== 200) {
+		flash.add(
+			`Failed to update course level '${courseLevel.name}': ${response.message}`,
+			"error"
+		);
+		return;
+	}
+	flash.add(
+		`Course level '${courseLevel.name}' updated successfully`,
+		"success"
+	);
+	editting.value = false;
+};
+
+const onDeleteClicked = () => {
+	modal.add({
+		title: "Delete Course level",
+		body: `Are you sure you want to delete course level '${courseLevel.value?.name}'? This will also delete all courses, questions, and answers associated with this course level and cannot be undone.`,
+		actions: [
+			{
+				text: "Cancel",
+				type: "neutral",
+			},
+			{
+				text: "Delete",
+				type: "danger",
+				action: deleteCourse,
+			},
+		],
+	});
+};
+
+const deleteCourse = async () => {
+	awaitingResponse.value = true;
+	const response = await courseLevelStore.actions.delete(props.id);
+	awaitingResponse.value = false;
+
+	if (response.status !== 200) {
+		flash.add(
+			`Failed to delete course level '${courseLevel.value?.name}': ${response.message}`,
+			"error"
+		);
+		return;
+	}
+	router.push({ name: "dashboard.course.index" });
+	const data = response.data as CourseLevel;
+	flash.add(`Course level '${data.name}' deleted successfully`, "success");
+};
+
+const editting = ref(false);
+
+onMounted(loadData);
+</script>
+
+<template>
+	<div v-if="loading" class="flex justify-center items-center h-full">
+		Loading...
+	</div>
+	<template v-else>
+		<template v-if="editting">
+			<course-level-form
+				:course-level="courseLevel"
+				:awaiting-response="awaitingResponse"
+				@cancel="editting = false"
+				@submit="updateCourseLevel" />
+		</template>
+		<template v-else>
+			<div class="flex justify-between items-center">
+				<h1 class="text-2xl font-bold">
+					{{ courseLevel.study_type }} - {{ courseLevel.name }}
+				</h1>
+				<div class="flex items-center">
+					<button
+						@click="editting = true"
+						class="px-4 py-2 bg-primary text-primary-text rounded-md font-bold">
+						Edit
+					</button>
+					<button
+						@click="onDeleteClicked"
+						class="px-4 py-2 bg-alert-warning text-alert-warning-text rounded-md font-bold">
+						Delete
+					</button>
+				</div>
+			</div>
+			<div class="mt-4 flex flex-col">
+				<div class="flex flex-row gap-1">
+					<span class="font-bold">Study Type</span>
+					<span>{{ courseLevel.study_type }}</span>
+				</div>
+
+				<div class="flex flex-row gap-1">
+					<span class="font-bold">Name</span>
+					<span>{{ courseLevel.name }}</span>
+				</div>
+
+				<div class="flex flex-row gap-1">
+					<span class="font-bold">Code</span>
+					<span>{{ courseLevel.code }}</span>
+				</div>
+			</div>
+		</template>
+	</template>
+</template>
+
+<style lang="scss" scoped></style>
