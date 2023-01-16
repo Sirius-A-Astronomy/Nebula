c37c2182f31385731e79981a038c6ef2f3a50675
Pieter Huizenga <mail@pieterhuizenga.com>
2023-01-16 14:46:17 +0100

    Create single app entry point

--- nebula/src/js/App.ts
+++ nebula/src/js/App.ts
@@ -1,8 +1,20 @@
-import { createApp } from "vue";
-
+import { fetchUser } from "@/stores/sessionStore";
 import App from "@/App.vue";
+import { createApp } from "vue";
+import router from "@/router";
 
-// This will eventually contain the entire app
-const app = createApp(App);
+// try to login, start as early as possible
+// using IIFE to have "top level" await
+(async () => {
+	try {
+		await fetchUser();
+	} catch (e) {
+		console.log("Not logged in", e);
+	}
 
-app.mount("#app");
+	const app = createApp(App);
+
+	app.use(router);
+
+	app.mount("#app");
+})();
--- nebula/src/js/App.vue
+++ nebula/src/js/App.vue
@@ -1,13 +1,53 @@
 <script setup lang="ts">
-// This will eventually contain the entire app
-
 import CookieNotice from "@components/ui/CookieNotice.vue";
+
+import "@scss/main.scss";
+import { useRoute } from "vue-router";
+import Dashboard from "@/DashboardLayout.vue";
+import BaseLayout from "@/BaseLayout.vue";
+
+import ModalContainer from "@components/ui/modals/ModalContainer.vue";
+import FlashContainer from "@components/ui/FlashContainer.vue";
+
+const route = useRoute();
 </script>
 
 <template>
 	<div>
+		<a href="#content" id="skip-link" class="skip-link sr-only"
+			>Skip to content</a
+		>
 		<CookieNotice />
+		<ModalContainer />
+		<FlashContainer />
+
+		<Dashboard v-if="route.name?.toString().startsWith('dashboard')" />
+		<BaseLayout v-else />
 	</div>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.skip-link,
+#skip-link {
+	position: absolute;
+	top: 8px;
+	left: 8px;
+	padding: 8px 16px;
+	z-index: 999;
+	border-radius: 8px;
+	font-size: 12px;
+	font-weight: bold;
+	text-decoration: none;
+	color: var(--color-primary-active);
+	box-shadow: var(--box-shadow);
+	background-color: var(--color-background);
+
+	&:focus {
+		height: auto;
+		width: auto;
+		clip: auto;
+		-webkit-clip-path: none;
+		clip-path: none;
+	}
+}
+</style>
