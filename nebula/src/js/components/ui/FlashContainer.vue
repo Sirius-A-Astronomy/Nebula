b2889fecf15121cdaa20405460c50cdc70f92717
Pieter Huizenga <mail@pieterhuizenga.com>
2023-01-16 14:47:09 +0100

    Create flashContainer

---
+++ nebula/src/js/components/ui/FlashContainer.vue
@@ -0,0 +1,40 @@
+<script setup lang="ts">
+import useFlash from "@stores/flashStore";
+
+const flash = useFlash();
+</script>
+
+<template>
+	<template v-if="flash.messages.value.length">
+		<div class="absolute top-0 right-0 m-4 flex flex-col gap-4 z-50">
+			<div
+				v-for="flashMessage in flash.messages.value"
+				:key="flashMessage.id"
+				class="p-4 rounded-md font-bold flex flex-row justify-between items-center gap-2"
+				:class="{
+					'bg-alert-error text-alert-error-text':
+						flashMessage.type === 'error',
+					'bg-alert-success text-alert-succes-text':
+						flashMessage.type === 'success',
+					'bg-alert-warning text-alert-warning-text':
+						flashMessage.type === 'warning',
+					'bg-alert-info text-alert-info-text':
+						flashMessage.type === 'info',
+				}">
+				<span>{{ flashMessage.message }}</span>
+				<button @click="flash.remove(flashMessage.id)">
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						height="24"
+						width="24"
+						fill="currentcolor">
+						<path
+							d="M6.4 19 5 17.6l5.6-5.6L5 6.4 6.4 5l5.6 5.6L17.6 5 19 6.4 13.4 12l5.6 5.6-1.4 1.4-5.6-5.6Z" />
+					</svg>
+				</button>
+			</div>
+		</div>
+	</template>
+</template>
+
+<style lang="scss" scoped></style>
