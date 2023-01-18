be489be4f147115ceb7f6428fd197419cab8dde1
Pieter Huizenga <mail@pieterhuizenga.com>
2023-01-18 13:17:28 +0100

    Improve comments vor vClickOutside

--- nebula/src/js/vue-services/directives/clickOutside.ts
+++ nebula/src/js/vue-services/directives/clickOutside.ts
@@ -2,16 +2,18 @@
 export const vClickOutside = {
 	beforeMount: (el, binding) => {
 		el.clickOutsideEvent = (event) => {
-			// here I check that click was outside the el and his children
+			// Check if the click was inside the element
 			if (el == event.target || el.contains(event.target)) {
 				return;
 			}
-			// and if it did, call method provided in attribute value
+
+			// if no exclude is provided, just run the handler
 			if (binding.value instanceof Function) {
 				binding.value();
 				return;
 			}
 
+			// Check if the click was inside an excluded element
 			for (const excluded of binding.value.exclude) {
 				const element = document.getElementById(excluded);
 				if (
