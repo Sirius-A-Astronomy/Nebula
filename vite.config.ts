8b70f16de1086d032d4f6be57aece01960eac84b
Pieter Huizenga <mail@pieterhuizenga.com>
2023-01-23 17:08:26 +0100

    Change build to only have 1 entry point

--- vite.config.ts
+++ vite.config.ts
@@ -6,73 +6,39 @@ import { resolve } from "node:path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	publicDir: "nebula/src/public",
-	base: "/static/",
-	build: {
-		manifest: true,
-		copyPublicDir: true,
+  publicDir: "nebula/src/public",
+  base: "/static/",
+  build: {
+    manifest: true,
+    copyPublicDir: true,
 
-		rollupOptions: {
-			input: {
-				add_question: resolve(
-					__dirname,
-					"nebula/src/js/add_question/add_question.ts"
-				),
-				main_scss: resolve(__dirname, "nebula/src/scss/main.scss"),
-
-				dashboard: resolve(
-					__dirname,
-					"nebula/src/js/dashboard/dashboardMain.ts"
-				),
-
-				// SCSS VIEWS
-				course_scss: resolve(
-					__dirname,
-					"nebula/src/scss/views/course.scss"
-				),
-				login_register_scss: resolve(
-					__dirname,
-					"nebula/src/scss/views/login_register.scss"
-				),
-				question_scss: resolve(
-					__dirname,
-					"nebula/src/scss/views/question.scss"
-				),
-				profile_scss: resolve(
-					__dirname,
-					"nebula/src/scss/views/profile.scss"
-				),
-			},
-			output: {
-				dir: "nebula/static",
-				entryFileNames: "[name].js",
-				chunkFileNames: "[name].js",
-				assetFileNames: "[name].[ext]",
-			},
-		},
-	},
-	plugins: [vue()],
-	resolve: {
-		alias: {
-			"@": fileURLToPath(new URL("./nebula/src/js", import.meta.url)),
-			"@scss": fileURLToPath(
-				new URL("./nebula/src/scss", import.meta.url)
-			),
-			"@views": fileURLToPath(
-				new URL("./nebula/src/js/views", import.meta.url)
-			),
-			"@components": fileURLToPath(
-				new URL("./nebula/src/js/components", import.meta.url)
-			),
-			"@stores": fileURLToPath(
-				new URL("./nebula/src/js/stores", import.meta.url)
-			),
-			"@http": fileURLToPath(
-				new URL("./nebula/src/js/http", import.meta.url)
-			),
-			// public files are copied to the root of the build directory and dev server
-			// so the vite alias differs from the tsconfig alias
-			"@public": fileURLToPath(new URL("./", import.meta.url)),
-		},
-	},
+    rollupOptions: {
+      input: {
+        app: resolve(__dirname, "nebula/src/js/app.ts"),
+      },
+      output: {
+        dir: "nebula/static",
+        entryFileNames: "[name].js",
+        chunkFileNames: "[name].js",
+        assetFileNames: "[name].[ext]",
+      },
+    },
+  },
+  plugins: [vue()],
+  resolve: {
+    alias: {
+      "@": fileURLToPath(new URL("./nebula/src/js", import.meta.url)),
+      "@scss": fileURLToPath(new URL("./nebula/src/scss", import.meta.url)),
+      "@views": fileURLToPath(
+        new URL("./nebula/src/js/views", import.meta.url)
+      ),
+      "@components": fileURLToPath(
+        new URL("./nebula/src/js/components", import.meta.url)
+      ),
+      "@stores": fileURLToPath(
+        new URL("./nebula/src/js/stores", import.meta.url)
+      ),
+      "@http": fileURLToPath(new URL("./nebula/src/js/http", import.meta.url)),
+    },
+  },
 });
