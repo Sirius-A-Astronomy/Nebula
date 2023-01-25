d8c012f3cb24f97fc5b755fc99fcf4a4465c29fb
Pieter Huizenga <mail@pieterhuizenga.com>
2023-01-25 13:57:55 +0100

    Format

--- tests/py/models/user_model_test.py
+++ tests/py/models/user_model_test.py
@@ -1,42 +1,44 @@
-from nebula.models.user import User, validate_username, validate_email
-from passlib.hash import sha256_crypt
 import pytest
+from passlib.hash import sha256_crypt
 from sqlalchemy.exc import IntegrityError
 
 from nebula import db
+from nebula.models.user import User, validate_email, validate_username
+
 
 def test_create_user(empty_app):
     with empty_app.app_context():
-        user = User(username='testuser', password=sha256_crypt.hash('testpassword'))
+        user = User(username="testuser", password=sha256_crypt.hash("testpassword"))
         db.session.add(user)
         db.session.commit()
-        assert user.username == 'testuser'
-        assert user.check_password('testpassword') == True
+        assert user.username == "testuser"
+        assert user.check_password("testpassword") == True
+
 
 def test_create_user_duplicate_username(empty_app):
     with empty_app.app_context():
-        user = User(username='testuser', password='testpassword')
+        user = User(username="testuser", password="testpassword")
         db.session.add(user)
         db.session.commit()
-        user2 = User(username='testuser', password='testpassword2')
+        user2 = User(username="testuser", password="testpassword2")
         db.session.add(user2)
         with pytest.raises(IntegrityError):
             db.session.commit()
 
+
 def test_check_password(empty_app):
     with empty_app.app_context():
-        user = User(username='testuser', password=sha256_crypt.hash('testpassword'))
+        user = User(username="testuser", password=sha256_crypt.hash("testpassword"))
         db.session.add(user)
         db.session.commit()
-        assert user.check_password('wrongpassword') == False
-        assert user.check_password('testpassword') == True
-
+        assert user.check_password("wrongpassword") == False
+        assert user.check_password("testpassword") == True
 
 
 class TestValidateUsername:
     def test_validate_username_with_spaces(self, empty_app):
         # arrange
-        username = 'user name'
+        username = "user name"
         user = None
 
         # act
@@ -48,7 +50,7 @@ class TestValidateUsername:
 
     def test_validate_username_with_special_characters(self, empty_app):
         # arrange
-        username = 'user@name'
+        username = "user@name"
         user = None
 
         # act
@@ -56,18 +58,21 @@ class TestValidateUsername:
             result = validate_username(username, user)
 
         # assert
-        assert result == (False, "Please enter a username without any special characters")
+        assert result == (
+            False,
+            "Please enter a username without any special characters",
+        )
 
     def test_validate_username_already_exists(self, empty_app):
         # arrange
-        username = 'username'
-        user_with_username = User(username=username, password='password')
+        username = "username"
+        user_with_username = User(username=username, password="password")
 
         with empty_app.app_context():
             db.session.add(user_with_username)
             db.session.commit()
 
-        # act
+            # act
             result = validate_username(username, None)
 
         # assert
@@ -78,7 +83,7 @@ class TestValidateUsername:
 
     def test_validate_username_valid(self, empty_app):
         # arrange
-        username = 'username'
+        username = "username"
         user = None
 
         # act
@@ -92,7 +97,7 @@ class TestValidateUsername:
 class TestValidateEmail:
     def test_validate_email_invalid_format(self, empty_app):
         # arrange
-        email = 'invalid_email'
+        email = "invalid_email"
         user = None
         with empty_app.app_context():
             # act
@@ -106,10 +111,12 @@ class TestValidateEmail:
 
     def test_validate_email_already_exists(self, empty_app):
         # arrange
-        email = 'valid@email.com'
+        email = "valid@email.com"
         user = None
         with empty_app.app_context():
-            user_with_email = User(email=email, password='not important', username='not important')
+            user_with_email = User(
+                email=email, password="not important", username="not important"
+            )
             db.session.add(user_with_email)
             db.session.commit()
             # act
@@ -123,7 +130,7 @@ class TestValidateEmail:
 
     def test_validate_email_valid(self, empty_app):
         # arrange
-        email = 'valid@email.com'
+        email = "valid@email.com"
         user = None
         with empty_app.app_context():
             # act
