4a0bba469d727cf4f332c962ecab012059a3b418
Pieter Huizenga <mail@pieterhuizenga.com>
2023-02-01 18:46:31 +0100

    Add some component tests

--- vite.config.ts
+++ vite.config.ts
@@ -6,39 +6,47 @@ import { resolve } from "node:path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  publicDir: "nebula/src/public",
-  base: "/static/",
-  build: {
-    manifest: true,
-    copyPublicDir: true,
+    publicDir: "nebula/src/public",
+    base: "/static/",
+    build: {
+        manifest: true,
+        copyPublicDir: true,
 
-    rollupOptions: {
-      input: {
-        app: resolve(__dirname, "nebula/src/js/App.ts"),
-      },
-      output: {
-        dir: "nebula/static",
-        entryFileNames: "[name].js",
-        chunkFileNames: "[name].js",
-        assetFileNames: "[name].[ext]",
-      },
+        rollupOptions: {
+            input: {
+                app: resolve(__dirname, "nebula/src/js/App.ts"),
+            },
+            output: {
+                dir: "nebula/static",
+                entryFileNames: "[name].js",
+                chunkFileNames: "[name].js",
+                assetFileNames: "[name].[ext]",
+            },
+        },
     },
-  },
-  plugins: [vue()],
-  resolve: {
-    alias: {
-      "@": fileURLToPath(new URL("./nebula/src/js", import.meta.url)),
-      "@scss": fileURLToPath(new URL("./nebula/src/scss", import.meta.url)),
-      "@views": fileURLToPath(
-        new URL("./nebula/src/js/views", import.meta.url)
-      ),
-      "@components": fileURLToPath(
-        new URL("./nebula/src/js/components", import.meta.url)
-      ),
-      "@stores": fileURLToPath(
-        new URL("./nebula/src/js/stores", import.meta.url)
-      ),
-      "@http": fileURLToPath(new URL("./nebula/src/js/http", import.meta.url)),
+    plugins: [vue()],
+    resolve: {
+        alias: {
+            "@": fileURLToPath(new URL("./nebula/src/js", import.meta.url)),
+            "@scss": fileURLToPath(
+                new URL("./nebula/src/scss", import.meta.url)
+            ),
+            "@views": fileURLToPath(
+                new URL("./nebula/src/js/views", import.meta.url)
+            ),
+            "@components": fileURLToPath(
+                new URL("./nebula/src/js/components", import.meta.url)
+            ),
+            "@stores": fileURLToPath(
+                new URL("./nebula/src/js/stores", import.meta.url)
+            ),
+            "@http": fileURLToPath(
+                new URL("./nebula/src/js/http", import.meta.url)
+            ),
+        },
     },
-  },
+
+    test: {
+        environment: "jsdom",
+    },
 });
