e465bce59f3ed1ad6a01847f38a7455d689e111a
Pieter Huizenga <mail@pieterhuizenga.com>
2023-02-02 18:16:36 +0100

    Add vitest types

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,10 @@
+/// <reference types="vitest" />
 import { fileURLToPath, URL } from "node:url";
 
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import { resolve } from "node:path";
 
-// https://vitejs.dev/config/
 export default defineConfig({
     publicDir: "nebula/src/public",
     base: "/static/",
