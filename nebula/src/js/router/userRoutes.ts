f594ac885f7f2a66f77048b7cee357f1e66878ab
Pieter Huizenga <mail@pieterhuizenga.com>
2023-03-02 09:22:12 +0100

    Add profile edit page

--- nebula/src/js/router/userRoutes.ts
+++ nebula/src/js/router/userRoutes.ts
@@ -16,6 +16,11 @@ export const userRoutes = [
         name: "user.profile",
         component: () => import("@/views/user/ProfileView.vue"),
     },
+    {
+        path: "/profile/edit",
+        name: "user.profile.edit",
+        component: () => import("@/views/user/ProfileEdit.vue"),
+    },
 ];
 
 export default userRoutes;
