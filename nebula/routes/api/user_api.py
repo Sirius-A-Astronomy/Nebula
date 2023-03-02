11ae799f64cdb22c8907c0eddae4996f7d41cbcb
Pieter Huizenga <mail@pieterhuizenga.com>
2023-03-02 09:21:27 +0100

    Improve user updating error handling, add ability to change password

--- nebula/routes/api/user_api.py
+++ nebula/routes/api/user_api.py
@@ -5,6 +5,10 @@ from flask_login import current_user, login_required
 
 from nebula import db
 from nebula.helpers.access_levels import ACCESS_LEVELS
+from nebula.helpers.error_bin import ErrorBin
+from nebula.models.answer import Answer
+from nebula.models.comment import Comment
+from nebula.models.question import Question
 from nebula.models.user import User, validate_email, validate_password
 from nebula.routes.api import bp as api_bp
 
@@ -56,38 +60,38 @@ def create_user_route():
     data = request.get_json(silent=True)
     email = data.get("email")
 
-    error_bin = {}
+    error_bin = ErrorBin()
     if email is None:
-        error_bin["email"] = "Email is required"
+        error_bin.add("email", "Email is required")
 
     if email:
         valid, message = validate_email(email)
         if not valid:
-            error_bin["email"] = message
+            error_bin.add("email", message)
 
     first_name = data.get("first_name")
     if not first_name:
-        error_bin["first_name"] = "First name is required"
+        error_bin.add("first_name", "First name is required")
 
     last_name = data.get("last_name")
     if not last_name:
-        error_bin["last_name"] = "Last name is required"
+        error_bin.add("last_name", "Last name is required")
 
     password = data.get("password")
     if not password:
-        error_bin["password"] = "Password is required"
+        error_bin.add("password", "Password is required")
 
     if password:
         valid, message = validate_password(email)
         if not valid:
-            error_bin["password"] = message
+            error_bin.add("password", message)
 
     password_confirmation = data.get("password_confirmation")
     if not password_confirmation:
-        error_bin["password_confirmation"] = "Password confirmation is required"
+        error_bin.add("password_confirmation", "Password confirmation is required")
 
     if password and password_confirmation and password != password_confirmation:
-        error_bin["password"] = "Passwords do not match"
+        error_bin.add("password_confirmation", "Passwords do not match")
 
     access_level = data.get("access_level")
     if access_level is not None:
@@ -122,14 +126,12 @@ def create_user_route():
     if access_level is None:
         access_level = 0
 
-    print(error_bin)
-
-    if error_bin:
+    if error_bin.has_errors():
         return (
             jsonify(
                 {
                     "message": "Some values were not entered correctly",
-                    "errors": error_bin,
+                    "errors": error_bin.get(),
                 }
             ),
             400,
@@ -197,20 +199,91 @@ def update_user(uuid):
 
     data = request.get_json(silent=True)
 
+    if (
+        (not data.get("email"))
+        and (not data.get("new_password"))
+        and (not data.get("first_name"))
+        and (not data.get("last_name"))
+        and (not data.get("access_level"))
+    ):
+        return jsonify({"message": "Nothing to update"}), 400
+
+    error_bin = ErrorBin()
+
     email = data.get("email")
-    if email is not None:
+    if email:
         (valid, message) = validate_email(email, user=user)
         if not valid:
-            return jsonify({"message": message}), 400
-        user.email = email
+            error_bin.add("email", message)
+
+    # user needs to enter password to change their own email
+    if email and str(current_user.uuid) == uuid and email != current_user.email:
+        current_password = data.get("current_password")
+        if not current_password:
+            error_bin.add(
+                "current_password", "Password is required to change email address"
+            )
+
+        if not user.check_password(current_password):
+            error_bin.add("current_password", "Password is incorrect")
+
+    new_password = data.get("new_password")
+    if new_password:
+        (valid, message) = validate_password(new_password)
+        if not valid:
+            error_bin.add("new_password", message)
+
+        current_password = data.get("current_password")
+        if not current_password:
+            error_bin.add(
+                "current_password",
+                "Please enter your current password to change your password",
+            )
+
+        if not user.check_password(current_password):
+            error_bin.add("current_password", "Password is incorrect")
+
+        password_confirmation = data.get("new_password_confirmation")
+        if not password_confirmation:
+            error_bin.add(
+                "new_password_confirmation", "Password confirmation is required"
+            )
+
+        if (
+            new_password
+            and password_confirmation
+            and new_password != password_confirmation
+        ):
+            error_bin.add("new_password_confirmation", "Passwords do not match")
+
+    if error_bin.has_errors():
+        return (
+            jsonify(
+                {
+                    "message": "Some values were not entered correctly",
+                    "errors": error_bin.get(),
+                }
+            ),
+            400,
+        )
 
     first_name = data.get("first_name")
-    if first_name is not None and first_name != user.first_name:
+    if first_name and first_name != user.first_name:
         user.first_name = first_name
 
     last_name = data.get("last_name")
-    if last_name is not None and last_name != user.last_name:
+    if last_name and last_name != user.last_name:
         user.last_name = last_name
+
+    if email and email != user.email:
+        user.email = email
+
+    if (
+        new_password
+        and data.get("current_password")
+        and data.get("new_password_confirmation")
+    ):
+        user.set_password(new_password)
 
     access_level = data.get("access_level")
     if access_level is not None and access_level != user.access_level:
